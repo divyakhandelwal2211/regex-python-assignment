4763201a4bd8097082aea78bcb50593ad3e05531
divya khandelwal <itsdivya2211@gmail.com>
2025-05-01 15:34:22 +0530

    assignment complete

--- loops2_assignment.py
+++ loops2_assignment.py
@@ -35,19 +35,99 @@
 
 """wap to find lcm of two number"""
 
-num1 = int(input("enter the number1: "))
-num2 = int(input("enter the number2: "))
+# num1 = int(input("enter the number1: "))
+# num2 = int(input("enter the number2: "))
 
-if num1 > num2:
-    max = num1
-else:
-    max = num2
+# if num1 > num2:
+#     max = num1
+# else:
+#     max = num2
 
-while True:
-    if max % num1 == 0 and max % num2 == 0:
-        lcm = max
-        break
+# while True:
+#     if max % num1 == 0 and max % num2 == 0:
+#         lcm = max
+#         break
+#     else:
+#         max += 1
+
+# print("lcm - ", lcm)
+
+
+"""wap to check number is armstrong or not"""
+
+# num = int(input("Enter the number: "))
+# temp = num
+
+# total_count = len(str(num))
+
+# total = 0
+# while int(num) > 0:
+#     digit = num % 10
+#     total += digit**total_count
+#     num //= 10
+
+
+# if total == temp:
+#     print("armstrong")
+# else:
+#     print("not armstrong")
+
+"""19.Write a program to check whether a number is Strong number or not
+a. Strong number is a special number whose sum of factorial digits is
+equal to the original number."""
+
+# num = int(input("Enter the number: "))
+
+# temp = num
+# total = 0
+# while num > 0:
+#     digit = num % 10
+
+#     factorial = 1
+#     for i in range(1, digit + 1):
+#         factorial *= i
+
+#     total += factorial
+#     num //= 10
+
+# if total == temp:
+#     print("strong number")
+# else:
+#     print("not strong number")
+
+
+"""wap to check the number is perfect or not"""
+
+# num = int(input("Enter the number: "))
+# temp = num
+# total = 0
+# for i in range(1, num):
+#     if num % i == 0:
+#         total += i
+
+# if total == temp:
+#     print("perfect number")
+# else:
+#     print("not a perfect number")
+
+
+"""wap of fibonacci series"""
+# num = int(input("enter the number of terms: "))
+# a = 0
+# b = 1
+# for i in range(num):
+#     print(a, end=" ")
+#     c = a + b
+#     a = b
+#     b = c
+
+
+"""wap to find ones complement of a number"""
+binary = input("enter the number in binary: ")
+ones_complement = ""
+for i in binary:
+    if i == "1":
+        ones_complement += "0"
     else:
-        max += 1
-
-print("lcm - ", lcm)
+        ones_complement += "1"
+print(ones_complement)
