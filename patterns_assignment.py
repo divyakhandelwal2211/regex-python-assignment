91f5d154f945fa9698012428064b128206c29a40
divya khandelwal <itsdivya2211@gmail.com>
2025-05-08 16:49:50 +0530

    some more assingment question done

--- patterns_assignment.py
+++ patterns_assignment.py
@@ -118,4 +118,394 @@ A B C D E"""
 #     for j in range(1, 5):
 #         print("*", end="")
 #     print()
-print("hello")
+
+"""   *
+     ***
+    *****
+   *******"""
+
+# for i in range(1, 5):
+#     for j in range(i, 4):
+#         print(" ", end="")
+#     for j in range(1, i):
+#         print("*", end="")
+#     for j in range(1, i + 1):
+#         print("*", end="")
+#     print()
+
+
+"""   *
+     * *
+    *   *
+   *******"""
+# for i in range(1, 5):
+#     for j in range(i, 4):
+#         print(" ", end="")
+#     for j in range(1, i):
+#         if j == 1 or i == 4:
+#             print("*", end="")
+#         else:
+#             print(" ", end="")
+#     for j in range(1, i + 1):
+#         if i == 4 or i == 1 or i == j:
+#             print("*", end="")
+#         else:
+#             print(" ", end="")
+#     print()
+
+
+""" *******
+     *****
+      ***
+       *"""
+# for i in range(1, 5):
+#     for j in range(1, i + 1):
+#         print(" ", end="")
+#     for j in range(i, 4):
+#         print("*", end="")
+#     for j in range(i, 5):
+#         print("*", end="")
+#     print()
+
+"""*
+   **
+   ***
+   ****
+   ***
+   **
+   *"""
+# for i in range(1, 4):
+#     for j in range(1, i + 1):
+#         print("*", end="")
+#     print()
+# for i in range(1, 5):
+#     for j in range(i, 5):
+#         print("*", end="")
+#     print()
+
+
+""" *
+   **
+  ***
+ ****
+  ***
+   **
+    *"""
+# for i in range(1, 4):
+#     for j in range(i, 5):
+#         print(" ", end="")
+#     for j in range(1, i + 1):
+#         print("*", end="")
+#     print()
+# for i in range(1, 5):
+#     for j in range(1, i + 1):
+#         print(" ", end="")
+#     for j in range(i, 5):
+#         print("*", end="")
+#     print()
+
+"""*
+  ***
+ *****
+*******
+ *****
+  ***
+   *"""
+# for i in range(1, 4):
+#     for j in range(i, 4):
+#         print(" ", end="")
+#     for j in range(1, i):
+#         print("*", end="")
+#     for j in range(1, i + 1):
+#         print("*", end="")
+#     print()
+# for i in range(1, 5):
+#     for j in range(1, i):
+#         print(" ", end="")
+#     for j in range(i, 4):
+#         print("*", end="")
+#     for j in range(i, 5):
+#         print("*", end="")
+#     print()
+
+""" 1
+   12
+  123
+ 1234
+12345"""
+# for i in range(1, 6):
+#     p = 1
+#     for j in range(1, i + 1):
+#         print(p, end="")
+#         p += 1
+#     print()
+
+"""12345
+   1234
+   123
+   12
+   1"""
+# for i in range(1, 6):
+#     p = 1
+#     for j in range(i, 6):
+#         print(p, end="")
+#         p += 1
+#     print()
+
+"""1 
+   1 2 
+   1   3
+   1     4
+   1 2 3 4 5"""
+# for i in range(1, 6):
+#     p = 1
+#     for j in range(1, i + 1):
+#         if j == 1 or i == 5 or i == j:
+#             print(p, end=" ")
+#         else:
+#             print(" ", end=" ")
+#         p += 1
+#     print()
+
+""" 1 
+   1 2 
+  1   3 
+ 1     4 
+1 2 3 4 5 """
+# for i in range(1, 6):
+#     for j in range(i, 5):
+#         print(" ", end="")
+#     p = 1
+#     for j in range(1, i + 1):
+#         if j == 1 or i == 5 or i == j:
+#             print(p, end=" ")
+#         else:
+#             print(" ", end=" ")
+#         p += 1
+#     print()
+
+
+"""     * 
+      * * * 
+    * * * * * 
+  * * * * * * * 
+* * * * * * * * * 
+  * * * * * * * 
+    * * * * * 
+      * * *
+        *"""
+# for i in range(1, 5):
+#     for j in range(i, 5):
+#         print(" ", end=" ")
+#     for j in range(1, i):
+#         print("*", end=" ")
+#     for j in range(1, i + 1):
+#         print("*", end=" ")
+#     print()
+
+# for i in range(1, 6):
+#     for j in range(1, i):
+#         print(" ", end=" ")
+#     for j in range(i, 5):
+#         print("*", end=" ")
+#     for j in range(i, 6):
+#         print("*", end=" ")
+#     print()
+
+"""     1
+      1 2 3
+    1 2 3 4 5
+  1 2 3 4 5 6 7
+1 2 3 4 5 6 7 8 9
+  1 2 3 4 5 6 7
+    1 2 3 4 5
+      1 2 3
+        1"""
+# for i in range(1, 5):
+#     for j in range(i, 5):
+#         print(" ", end=" ")
+#     p = 1
+#     for j in range(1, 2 * i):
+#         print(p, end=" ")
+#         p += 1
+#     print()
+
+# for i in range(5, 0, -1):
+#     for j in range(5, i, -1):
+#         print(" ", end=" ")
+#     for j in range(1, 2 * i):
+#         print(j, end=" ")
+#     print()
+
+"""1 2 3 4 5 6 7 8 9 
+    1 2 3 4 5 6 7 
+      1 2 3 4 5
+        1 2 3
+          1
+        1 2 3
+      1 2 3 4 5
+    1 2 3 4 5 6 7
+  1 2 3 4 5 6 7 8 9"""
+# for i in range(5, 1, -1):
+#     for j in range(5, i, -1):
+#         print(" ", end=" ")
+#     for j in range(1, 2 * i):
+#         print(j, end=" ")
+#     print()
+# for i in range(1, 6):
+#     for j in range(i, 5):
+#         print(" ", end=" ")
+#     for j in range(1, 2 * i):
+#         print(j, end=" ")
+#     print()
+
+"""A B C D E F G H I 
+    A B C D E F G
+      A B C D E
+        A B C
+          A
+        A B C
+      A B C D E
+    A B C D E F G
+  A B C D E F G H I"""
+# for i in range(5, 1, -1):
+#     for j in range(5, i, -1):
+#         print(" ", end=" ")
+#     p = 65
+#     for j in range(1, 2 * i):
+#         print(chr(p), end=" ")
+#         p += 1
+#     print()
+# for i in range(1, 6):
+#     for j in range(i, 5):
+#         print(" ", end=" ")
+#     p = 65
+#     for j in range(1, 2 * i):
+#         print(chr(p), end=" ")
+#         p += 1
+#     print()
+
+""" * * * * * 
+    *       * 
+    *       *
+    *       *
+    * * * * *"""
+# for i in range(1, 6):
+#     for j in range(1, 6):
+#         if i == 1 or j == 1 or j == 5 or i == 5:
+#             print("*", end=" ")
+#         else:
+#             print(" ", end=" ")
+
+#     print()
+
+""" 1 2 3 4 5 
+    1       5 
+    1       5
+    1       5
+    1 2 3 4 5"""
+# for i in range(1, 6):
+#     for j in range(1, 6):
+#         if i == 1 or j == 1 or j == 5 or i == 5:
+#             print(j, end=" ")
+#         else:
+#             print(" ", end=" ")
+
+#     print()
+
+""" A B C D E 
+    A       E 
+    A       E
+    A       E
+    A B C D E"""
+# for i in range(1, 6):
+#     p = 65
+#     for j in range(1, 6):
+#         if i == 1 or j == 1 or j == 5 or i == 5:
+#             print(chr(p), end=" ")
+#         else:
+#             print(" ", end=" ")
+#         p += 1
+# print()
+
+"""     * 
+      *   * 
+    *       * 
+  *           * 
+*               *      
+  *           *        
+    *       *
+      *   *
+        *"""
+# for i in range(1, 5):
+#     for j in range(i, 5):
+#         print(" ", end=" ")
+#     for j in range(1, 2 * i):
+#         if j == 1 or j == 2 * i - 1:
+#             print("*", end=" ")
+#         else:
+#             print(" ", end=" ")
+#     print()
+
+# for i in range(5, 0, -1):
+#     for j in range(5, i, -1):
+#         print(" ", end=" ")
+#     for j in range(1, 2 * i):
+#         if j == 1 or j == 2 * i - 1:
+#             print("*", end=" ")
+#         else:
+#             print(" ", end=" ")
+#     print()
+
+"""     A 
+      A   C 
+    A       E
+  A           G        
+A               I      
+  A           G        
+    A       E
+      A   C
+        A"""
+# for i in range(1, 5):
+#     for j in range(i, 5):
+#         print(" ", end=" ")
+#     p = 65
+#     for j in range(1, 2 * i):
+#         if j == 1 or j == 2 * i - 1:
+#             print(chr(p), end=" ")
+#         else:
+#             print(" ", end=" ")
+#         p += 1
+#     print()
+
+# for i in range(5, 0, -1):
+#     for j in range(5, i, -1):
+#         print(" ", end=" ")
+#     p = 65
+#     for j in range(1, 2 * i):
+#         if j == 1 or j == 2 * i - 1:
+#             print(chr(p), end=" ")
+#         else:
+#             print(" ", end=" ")
+#         p += 1
+#     print()
+
+"""******
+   *    *
+   *    *
+   ******"""
+# for i in range(1, 5):
+#     for j in range(1, 7):
+#         if i == 1 or i == 4 or j == 1 or j == 6:
+#             print("*", end="")
+#         else:
+#             print(" ", end="")
+#     print()
+
+
+for i in range(1, 6):
+    p = 69
+    for j in range(1, i + 1):
+        print(chr(p), end=" ")
+        p += 1
+    print()
