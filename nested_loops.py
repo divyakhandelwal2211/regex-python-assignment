cb00dd5bfa7519e6900f4ce8ffca939850b480cf
divya khandelwal <itsdivya2211@gmail.com>
2025-05-08 16:39:26 +0530

    pattern part complete

--- nested_loops.py
+++ nested_loops.py
@@ -196,123 +196,90 @@
 #         print("*", end=" ")
 #     print("")
 
-"""A 
-   A B 
-   A B C
-   A B C D
-   A B C D E"""
 
-# for i in range(1, 6):
-#     for j in range(i):
-#         print(chr(65 + j), end=" ")
-
-#     print("")
-
-"""A 
-B B 
-C C C
-D D D D
-E E E E E"""
-
-# for i in range(5):
-#     for j in range(1, i + 2):
-#         print(chr(65 + i), end=" ")
-
-#     print("")
-
-"""       * 
-        * * 
-      * * *
-    * * * *
-  * * * * *"""
-
-# for i in range(1, 6):
-#     for j in range(i, 6):
-#         print(" ", end=" ")
+"""---*
+   --**
+   -*?*
+   ****"""
+# for i in range(1, 5):
+#     for j in range(i, 4):
+#         print("-", end="")
 #     for j in range(1, i + 1):
-#         print("*", end=" ")
-#     print("")
-
-"""       1 
-        1 2 
-      1 2 3
-    1 2 3 4
-  1 2 3 4 5"""
-# for i in range(1, 6):
-#     for j in range(i, 6):
-#         print(" ", end=" ")
-#     for j in range(1, i + 1):
-#         print(j, end=" ")
-#     print("")
-
-"""1 2 3 4 5 
-     1 2 3 4 
-       1 2 3
-         1 2
-           1"""
-# for i in range(1, 6):
-#     for j in range(1, i + 1):
-#         print(" ", end=" ")
-#     for j in range(1, 7 - i):
-#         print(j, end=" ")
+#         if j == 1 or i == 4 or j == i:
+#             print("*", end="")
+#         else:
+#             print("?", end="")
 #     print()
 
-""" A B C D E 
-      A B C D 
-        A B C
-          A B
-            A"""
+"""*****
+   -*  *
+   --* *
+   ---**
+   ----*"""
 # for i in range(1, 6):
-#     for j in range(1, i + 1):
-#         print(" ", end=" ")
-#     for j in range(0, 6 - i):
-#         print(chr(65 + j), end=" ")
+#  for j in range(1, i):
+#      print("-", end="")
+#  for j in range(i, 6):
+#      if j == 5 or j == i or i == 1:
+#          print("*", end="")
+#      else:
+#          print(" ", end="")
+
+#  print()
+
+"""abcd
+   1abc
+   12ab
+   123a"""
+# for i in range(1, 5):
+#     for j in range(1, i):
+#         print(j, end="")
+#     p = 97
+#     for j in range(i, 5):
+#         print(chr(p), end="")
+#         p += 1
 #     print()
 
-"""  * * * * * 
-    * * * * *
-   * * * * *
-  * * * * *
- * * * * *"""
-# for i in range(1, 6):
-#     for j in range(i, 6):
-#         print(" ", end="")
-#     for j in range(1, 6):
+"""---* 
+   --* * 
+   -* * * 
+   * * * *"""
+
+# for i in range(1, 5):
+#     for j in range(i, 4):
+#         print("-", end="")
+#     for k in range(1, i + 1):
 #         print("*", end=" ")
-#     print("")
 
-"""  1 2 3 4 5 
-    1 2 3 4 5
-   1 2 3 4 5
-  1 2 3 4 5
- 1 2 3 4 5"""
-# for i in range(1, 6):
-#     for j in range(i, 6):
-#         print(" ", end="")
-#     for j in range(1, 6):
-#         print(j, end=" ")
-#     print("")
+#     print()
 
-
-"""  A B C D E
-    A B C D E
-   A B C D E
-  A B C D E
- A B C D E"""
-# for i in range(1, 6):
-#     for j in range(i, 6):
-#         print(" ", end="")
-#     for j in range(5):
-#         print(chr(65 + j), end=" ")
-#     print("")
-
-"""****
-    ****
-     ****
-      ****"""
+"""* * * * 
+    * * * 
+     * * 
+      *"""
 # for i in range(1, 5):
 #     for j in range(1, i):
 #         print(" ", end="")
-#     for j in range(1, 5):
-#         print("*", end="")
+#     for j in range(i, 5):
+#         print("*", end=" ")
 #     print()
+
+
+"""----1 
+   ---1 2 
+   --1 2 3
+   -1 2 3 4
+   1 2 3 4 5"""
+# for i in range(1, 6):
+#     for j in range(i, 5):
+#         print("-", end="")
+#     p = 1
+#     for j in range(1, i + 1):
+#         print(p, end=" ")
+#         p += 1
+#     print()
+
+for i in range(1, 5):
+    for j in range(1, 5 + 1 - i):
+        print(j, end=" ")
+    print()
